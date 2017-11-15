ea9e96b73876cec40c28c13ee6f68f0c7331df4f
caussepa <caussepa@gmail.com>
2017-11-15 19:46:01 +0000

    started the client side

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
    console.log('MONGO DB CONNECTED!!');
  
   //connect to Socket.io
-  client.on('connection', function(){
+  client.on('connection', function(socket){
     let chat = db.collection('chats');
     
     sendStatus = function(s){
