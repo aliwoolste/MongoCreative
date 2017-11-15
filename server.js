29608f8a4183446037cf8d4d8622a7f7a29fde23
caussepa <caussepa@gmail.com>
2017-11-15 18:59:37 +0000

    fixed the server.js file

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
   if(err){
       throw err;
   }
- });
-  
-  console.log('MONGO DB CONNECTED!!);
-  
+ 
+   console.log('MONGO DB CONNECTED!!');
+ 
   //connect to Socket.io
   client.on('connection', function(){
     let chat = db.collection('chats');
@@ -17,30 +16,45 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
     sendStatus = function(s){
         socket.emit('status',s);
     }
-   });
     
-    //get chats from mongo collection
+        //get chats from mongo collection
     chat.find().sort({_id:1}).toArray(function(err, res){
       if(err) {
         throw err;
       }
       //if not, emit messages to index file
       socket.emit('output', res);
-  });
-    
+    });
+
     socket.on('input', function(data){
       let name = data.name;
       let message = data.message;
-      
-      chat.insert({name: name; message: message}, function(){
-        client.emit('output',[data]);
-        
-        //clear messages
-        socket.on('clear', function(data){
-          chat.remove({}), function() {
-            socket.emit('cleared');
-            
-         });
+
+      if(name == '' || message == '')
+      {
+        sendStatus("Please enter a name AND a message")
+      }
+      else{
+        chat.insert({name: name, message: message}, function(){
+          client.emit('output',[data]);
+
+          sendStatus({
+            message: "Message sent",
+            clear:true
+          });
         });
+      }
+    });
+
+    //clear messages
+     socket.on('clear', function(data){
+       chat.remove({}, function() {
+         socket.emit('cleared');
+       });
+     });
+
+   });
+ 
+ });
   
     
